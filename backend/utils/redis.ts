4a65c26c3bdd134ae2c9d284c9703bc1ebae8bd6
Fasna123 <fasnanishar@gmail.com>
2024-06-28 15:01:33 +0530

    redis updates

--- backend/utils/redis.ts
+++ backend/utils/redis.ts
@@ -4,13 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const redisClient = () => {
-    if (process.env.REDIS_URL) {
-        console.log("Redis Connected");
-        return process.env.REDIS_URL;
-    }
-    throw new Error("Redis connection failed");
+  if (process.env.REDIS_URL) {
+    console.log("Redis Connected");
+    return process.env.REDIS_URL;
+  }
+  throw new Error("Redis connection failed");
 };
 
 export const redis = new Redis(redisClient());
-
-
