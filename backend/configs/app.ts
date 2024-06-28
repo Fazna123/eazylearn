dc65f65b63c03b9447eeb13d89e0b257bea8f9bf
Fasna123 <fasnanishar@gmail.com>
2024-06-29 00:31:06 +0530

    debugged

--- backend/configs/app.ts
+++ backend/configs/app.ts
@@ -13,16 +13,16 @@ const createServer = () => {
   const app = express();
   const server = http.createServer(app);
 
-  // if (process.env.NODE_ENV === "development") {
-  //   app.use(
-  //     cors({
-  //       origin: process.env.ORIGIN,
-  //       methods: ["GET", "POST", "PUT", "DELETE"],
-  //       credentials: true,
-  //     })
-  //   );
-  // }
-  app.use(cors());
+  if (process.env.NODE_ENV === "development") {
+    app.use(
+      cors({
+        origin: process.env.ORIGIN,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+      })
+    );
+  }
+  //app.use(cors());
   const io = new SocketIOServer(server, {
     cors: {
       origin: "*",
