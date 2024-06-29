9b17acd439886802f8278bf653bd2cd6927189b1
Fasna123 <fasnanishar@gmail.com>
2024-06-29 10:20:53 +0530

    minor changes

--- backend/build/configs/app.js
+++ backend/build/configs/app.js
@@ -17,9 +17,22 @@ const createServer = () => {
     const server = http_1.default.createServer(app);
     app.use((0, cors_1.default)({
         origin: process.env.ORIGIN,
-        methods: ["GET", "POST", "PUT", "DELETE"],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
         credentials: true,
     }));
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "http://www.eazylearn.xyz");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        next();
+    });
+    // Handle preflight requests
+    app.options("*", (req, res) => {
+        res.header("Access-Control-Allow-Origin", "http://www.eazylearn.xyz");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.sendStatus(200);
+    });
     // if (process.env.NODE_ENV === "development") {
     //   app.use(
     //     cors({
