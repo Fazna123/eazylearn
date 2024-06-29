4a5e076f75c39fac5c82c3de692b006ec7aa2bbf
Fasna123 <fasnanishar@gmail.com>
2024-06-29 12:35:27 +0530

    vite changed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,42 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.BACKEND_URL,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
+
+// console.log(process.env.BACKEND_URL);
+// https://vitejs.dev/config/
+// export default defineConfig({
+
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: import.meta.env.BACKEND_URL,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
 
 // export default defineConfig({
 //   plugins: [react()],
