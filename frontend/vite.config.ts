a4ab037f620ae7f084842da8814005f61f92f0fa
Fasna123 <fasnanishar@gmail.com>
2024-06-29 20:00:25 +0530

    vite changed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,37 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: env.BACKEND_URL,
-          changeOrigin: true,
-          secure: false,
-        },
+// console.log(process.env.BACKEND_URL);
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://backend.eazylearn.xyz",
+        secure: false,
       },
     },
-  };
+  },
 });
+
+//export default defineConfig(({ mode }) => {
+// Load env file based on `mode` in the current working directory.
+// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+//const env = loadEnv(mode, process.cwd(), "");
+
+//return {
+//plugins: [react()],
+// server: {
+//   proxy: {
+//     "/api": {
+//       target: env.BACKEND_URL,
+//       changeOrigin: true,
+//       secure: false,
+//     },
+//   },
+// },
+//};
+//});
 
 // https://vitejs.dev/config/
