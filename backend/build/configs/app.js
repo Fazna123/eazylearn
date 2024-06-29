68b4be1aa6d40879f35a0d5105d1b089e00d5197
Fasna123 <fasnanishar@gmail.com>
2024-06-29 09:55:53 +0530

    fixed minor changes

--- backend/build/configs/app.js
+++ backend/build/configs/app.js
@@ -15,13 +15,20 @@ const socket_io_1 = require("socket.io");
 const createServer = () => {
     const app = (0, express_1.default)();
     const server = http_1.default.createServer(app);
-    if (process.env.NODE_ENV === "development") {
-        app.use((0, cors_1.default)({
-            origin: process.env.ORIGIN,
-            methods: ["GET", "POST", "PUT", "DELETE"],
-            credentials: true,
-        }));
-    }
+    app.use((0, cors_1.default)({
+        origin: process.env.ORIGIN,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    }));
+    // if (process.env.NODE_ENV === "development") {
+    //   app.use(
+    //     cors({
+    //       origin: process.env.ORIGIN,
+    //       methods: ["GET", "POST", "PUT", "DELETE"],
+    //       credentials: true,
+    //     })
+    //   );
+    // }
     //app.use(cors());
     const io = new socket_io_1.Server(server, {
         cors: {
